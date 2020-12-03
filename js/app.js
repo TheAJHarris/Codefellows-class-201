293ed5d4555fe3abac534b913a3e589a2a0d8f57
Anthony Johnson <theanthonyjohnson@gmail.com>
2020-12-02 20:33:39 -0500

    fixed some errors

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
-'use strict'
+'use strict';
 
-var userName = prompt('Hello! What is your name?')
-alert('Hi,' + userName + ', thanks for coming to my page!')
+var userName = prompt('Hello! What is your name?');
+alert(`Hi, ${userName}, thanks for coming to my page!`);
 
-var name = prompt('My full name is Anthony Johnson, do people call me AJ for short?').toLowerCase();
+var myName = prompt('My full name is Anthony Johnson, do people call me AJ for short?').toLowerCase();
 // console.log(name)
 
-if (name === 'yes' || name === 'y') {
-  alert('yes indeed')
+if (myName === 'yes' || myName === 'y') {
+  alert('yes indeed');
 }
 
 var homeTown = prompt('I am from NYC, Staten island to be exact, do people call it the forgotten borough?').toLowerCase();
 // console.log(homeTown)
 
 if (homeTown === 'yes' || homeTown === 'y') {
-  alert('yes, but I call it home!')
+  alert('yes, but I call it home!');
 }
 
 var military = prompt('I served in the military, but spent a good portion of my time on water, was I in the navy?').toLowerCase();
 // console.log(military)
 
 if (military === 'yes' || military === 'y') {
-  alert('correct! Haze gray and underway!')
+  alert('correct! Haze gray and underway!');
 }
 
 var education = prompt('I went to college in New Orleans, is this also where mardi gras happens?').toLowerCase();
 // console.log(education)
 
 if (education === 'yes' || education === 'y') {
-  alert('yeah it is!  One of the best parades I have been to!')
+  alert('yeah it is!  One of the best parades I have been to!');
 }
 
 var dreamJob = prompt('Of all the places I have in mind for work, the islands are my DREAM!').toLowerCase();
 // console.log(dreamJob)
 
 if (dreamJob === 'yes' || dreamJob === 'y') {
-  alert('You bet! Can you say paradise?')
+  alert('You bet! Can you say paradise?');
 }
 
 for (var i = 0; i < 4; i++) {
@@ -68,5 +68,5 @@ for (var i = 0; i < 4; i++) {
   }
 }
 
-alert('Thanks for learning a bit about me ' + userName + ', hope you have fun!')
+alert('Thanks for learning a bit about me ' + userName + ', hope you have fun!');
 
